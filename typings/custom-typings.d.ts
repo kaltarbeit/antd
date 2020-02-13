cc1eb6dfa73bb7ae8aa0b89af59f97cbab88e1e7
langerhans <langerhans@kaltarbeit.com>
2020-02-13 11:45:29 +0900

    change sub package

--- typings/custom-typings.d.ts
+++ typings/custom-typings.d.ts
@@ -12,6 +12,8 @@ declare module 'rc-calendar*';
 
 declare module 'rc-time-picker*';
 
+declare module 'rc-pagination/*';
+
 declare module 'omit.js';
 
 declare module 'rc-animate';
@@ -19,6 +21,8 @@ declare module 'rc-animate';
 declare module 'rc-util*';
 
 declare module 'css-animation*';
+
+declare module 'rc-select';
 
 declare module 'rc-cascader';
 
@@ -83,9 +87,3 @@ declare module 'react-copy-to-clipboard';
 
 // https://github.com/ant-design/ant-design/pull/19254
 declare module 'react-lifecycles-compat';
-
-declare module '@kaltarbeit/rc-pagination';
-declare module '@kaltarbeit/rc-pagination/*';
-declare module 'rc-pagination/*';
-
-declare module '@kaltarbeit/rc-select';
